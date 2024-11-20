bf37e35763745f6516b0e40d5b42df614b6a3a75
KaCha <nnktran.0703@gmail.com>
2024-11-20 10:38:56 +0700

    Tran_Update_RangBuoc

--- Backend/DB_QLSushiX.sql
+++ Backend/DB_QLSushiX.sql
@@ -219,7 +219,7 @@ create table LichSuLamViec (
 );
 go
 -- Tạo khóa ngoại và ràng buộc:
-
+-- Tạo khóa ngoại:
 -- Bảng Chi Nhánh:
 alter table ChiNhanh
 	add constraint FK_CN_MaQuanLi_NV_NhanVien
@@ -337,6 +337,137 @@ alter table LichSuLamViec
 	foreign key (LSLV_MaNhanVien)
 	references NhanVien(NV_MaNhanVien)
 
+-- Ràng buộc: 
+-- Bảng Chi Nhánh:
+alter table ChiNhanh
+	add constraint C_ChiNhanh_ThoiGian
+	check (CN_TGMoCua < CN_TGDongCua)
+
+alter table ChiNhanh
+	add constraint C_ChiNhanh_BaiXeMay
+	check (CN_BaiDoXeMay in (0, 1))
+
+alter table ChiNhanh
+	add constraint C_ChiNhanh_BaiXeOto
+	check (CN_BaiDoXeOto in (0, 1))
+
+alter table ChiNhanh
+	add constraint C_ChiNhanh_HoTroGiaoHang
+	check (CN_HoTroGiaoHang in (0, 1))
+
+-- Bảng Món ăn:
+alter table MonAn
+	add constraint C_MonAn_CoSan
+	check (MA_CoSan in (0, 1))
+
+alter table MonAn
+	add constraint C_MonAn_HoTroGiaoHang
+	check (MA_HoTroGiaoHang in (0, 1))
+
+alter table MonAn
+	add constraint C_MonAn_KhauPhanAn
+	check (MA_KhauPhan > 0)
+
+-- Bảng món được đặt:
+alter table MonDuocDat
+	add constraint C_MonDuocDat_SoLuong
+	check (MDD_SoLuong >0)
+
+-- Bảng Đặt trực tiếp
+alter table DatTrucTiep
+	add constraint C_DTT_SoBan
+	check (DTT_SoBan > 0)
+
+alter table DatTrucTiep
+	add constraint C_DTT_SoLuongKH
+	check (DTT_SoLuongKH > 0)
+
+-- Bảng Đặt trước
+alter table DatTruoc
+	add constraint C_DT_SoBan
+	check (DT_SoBan > 0 )
+
+alter table DatTruoc
+	add constraint C_DT_SoLuongKH
+	check (DT_SoLuongKH > 0)
+
+-- Bảng khách hàng
+alter table KhachHang
+	add constraint C_KH_GioiTinh
+	check (KH_GioiTinh in ('Nam', N'Nữ'))
+
+-- Bảng Loại thẻ
+alter table LoaiThe
+	add constraint C_LT_TenLoaiThe
+	check (LT_TenLoaiThe in ('Membership', 'Silver', 'Gold'))
+
+-- Bảng Thẻ Thành Viên
+alter table TheThanhVien
+	add constraint C_TTV_SoNamSuDung
+	check (TTV_SoNamSuDung > 0)
+
+alter table TheThanhVien
+	add constraint C_TTV_DiemTichLuy
+	check (TTV_DiemTichLuy > 0)
+
+alter table TheThanhVien
+	add constraint C_TTV_TrangThai
+	check (TTV_TrangThai in (N'Mở', N'Đóng'))
+
+-- Khuyến mãi
+alter table KhuyenMai
+	add constraint C_KM_TyLeGiamGia
+	check (KM_TyLeGiamGia > 0 and KM_TyLeGiamGia < 1)
+
+
+-- Hóa đơn:
+alter table HoaDon
+	add constraint C_HD_Tien
+	check ( HD_SoTienGiam > 0 and HD_SoTienGiam < HD_TongTruocGiam  and HD_SoTienGiam < HD_TongTienThanhToan)
+
+-- Bảng Phiếu đánh giá:
+alter table PhieuDanhGia
+	add constraint C_PDG_DiemPhucVu
+	check (DG_DiemPhucVu > 0 and DG_DiemPhucVu < 6)
+
+alter table PhieuDanhGia
+	add constraint C_PDG_DiemChiNhanh
+	check (DG_DiemChiNhanh > 0 and DG_DiemChiNhanh < 6)
+
+alter table PhieuDanhGia
+	add constraint C_PDG_DiemCLMonAn
+	check (DG_DiemCLMonAn > 0 and DG_DiemCLMonAn < 6)
+
+alter table PhieuDanhGia
+	add constraint C_PDG_DiemGiaCa
+	check (DG_DiemGiaCa > 0 and DG_DiemGiaCa < 6)
+
+alter table PhieuDanhGia
+	add constraint C_PDG_DiemKhongGian
+	check (DG_DiemKhongGian > 0 and DG_DiemKhongGian < 6)
+
+-- Bảng Nhân Viên:
+alter table NhanVien
+	add constraint C_NV_ThoiGian
+	check (NV_NgayVaoLam < NV_NgayNghiViec)
+
+alter table NhanVien
+	add constraint C_NV_GioiTinh
+	check (NV_GioiTinh in ('Nam', N'Nữ'))
+
+alter table NhanVien
+	add constraint C_NV_NgaySinh
+	check (NV_NgaySinh < NV_NgayVaoLam)
+
+-- Bảng Bộ phận của nhân viên
+alter table BoPhan_NhanVien
+	add constraint C_BP_NV_Luong
+	check (BP_NV_Luong > 0.0)
+
+-- Bảng lịch sử làm việc
+alter table LichSuLamViec
+	add constraint C_LSLV_ThoiGian
+	check (LSLV_NgayBatDau < LSLV_NgayKetThuc)
 -- Xóa database:
 -- use master;
 -- drop database DB_SushiX;
