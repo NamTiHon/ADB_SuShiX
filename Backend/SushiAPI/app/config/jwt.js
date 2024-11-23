ed17b59c4a7f2dcb55ab99b4901c0be0aee93e1a
Nguyễn Trọng Trí <nguyentrongtri.hcmus@gmail.com>
2024-11-23 19:13:28 +0700

    su dung dotenv

--- Backend/SushiAPI/app/config/jwt.js
+++ Backend/SushiAPI/app/config/jwt.js
@@ -1,9 +1,8 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-//const JWT_SECRET = process.env.JWT_SECRET;
-//console.log('JWT_SECRET:', process.env.JWT_SECRET);  // Should print 'strong-jwt-secret-key'
-
-const JWT_SECRET = 'strong-jwt-secret-key';  // JWT secret key
+const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined in environment variables');  // Early error check
--- Backend/SushiAPI/server.js
+++ Backend/SushiAPI/server.js
@@ -1,5 +1,10 @@
 import app from './app/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const PORT = process.env.PORT || 5000; 
+
+console.log(PORT);
 
 console.log('PORT:', PORT);
 
