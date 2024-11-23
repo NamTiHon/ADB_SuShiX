5ba8c127e654d9677ad9686d7ea3f73a88bb1583
PC <tringuyen.thirtyfive@gmail.com>
2024-11-23 21:07:37 +0700

    Update App.js

--- USER/src/App.js
+++ USER/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/components/Home';
 import Menu from './pages/components/Menu';
 import Login from './pages/components/Login';
+import BranchSearch from './pages/components/BranchSearch';
+import CustomerInfo from './pages/components/CustomerInfo';
 import { UserProvider } from './context/UserContext';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/branch" element={<BranchSearch />} />
+          <Route path="/customer-info" element={<CustomerInfo />} />
         </Routes>
       </Router>
     </UserProvider>
