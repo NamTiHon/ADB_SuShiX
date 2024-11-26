806cfa562052e97e2c685629b9fd17dbcefa907e
KaCha <nnktran.0703@gmail.com>
2024-11-27 00:20:24 +0700

    KhanhTran_UPDATE_HomeAdmin

--- ADMIN/src/pages/components/Home.jsx
+++ ADMIN/src/pages/components/Home.jsx
@@ -6,14 +6,20 @@ function Home(){
     return (
         <div className="homePage">
             <Nav />
-            <aside className="sideBar">
-                <ul>
-                    <li>Quản lý nhân viên</li>
-                    <li>Quản lý khách hàng</li>
-                    <li>Danh sách đặt bàn</li>
-                    <li>Danh sách đặt món oline</li>
-                </ul>
-            </aside>
+            <div className="adminContainer">
+                <aside className="sideBar">
+                    <ul>
+                        <li>Quản lý nhân viên</li>
+                        <li>Quản lý khách hàng</li>
+                        <li>Danh sách đặt bàn</li>
+                        <li>Danh sách đặt món oline</li>
+                    </ul>
+                </aside>
+                <div className="mainContent">
+                    <h1>Chào mừng, Admin!</h1>
+                </div>
+            </div>
+            
         </div>
     );
 }
